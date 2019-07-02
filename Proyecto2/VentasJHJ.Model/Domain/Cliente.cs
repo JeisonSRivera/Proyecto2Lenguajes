444e049732223ff67f67b5dc3ff705385494ca0a
JeisonSRivera <jeisonsegura15@gmail.com>
2019-07-02 16:16:48 -0600

    4:20

--- Proyecto2/VentasJHJ.Model/Domain/Cliente.cs
+++ Proyecto2/VentasJHJ.Model/Domain/Cliente.cs
@@ -11,7 +11,6 @@ namespace VentasJHJ.Model.Domain
         private String apellidosCliente;
         private String cedulaCliente;
         private String emailCliente;
-        private String passwordCliente;
         private String direccionCliente;
         private String telefonoCliente;
         List<PosOrden> ordenes;
@@ -22,25 +21,23 @@ namespace VentasJHJ.Model.Domain
         }
 
         public Cliente(string nombreCliente, string apellidosCliente, string cedulaCliente, string emailCliente, 
-            string passwordCliente, string direccionCliente, string telefonoCliente)
+             string direccionCliente, string telefonoCliente)
         {
             this.nombreCliente = nombreCliente;
             this.apellidosCliente = apellidosCliente;
             this.cedulaCliente = cedulaCliente;
             this.emailCliente = emailCliente;
-            this.passwordCliente = passwordCliente;
             this.direccionCliente = direccionCliente;
             this.telefonoCliente = telefonoCliente;
         }
 
-        public Cliente(int idCliente, string nombreCliente, string apellidosCliente, string cedulaCliente, string emailCliente, string passwordCliente, string direccionCliente, string telefonoCliente)
+        public Cliente(int idCliente, string nombreCliente, string apellidosCliente, string cedulaCliente, string emailCliente, string direccionCliente, string telefonoCliente)
         {
             this.idCliente = idCliente;
             this.nombreCliente = nombreCliente;
             this.apellidosCliente = apellidosCliente;
             this.cedulaCliente = cedulaCliente;
             this.emailCliente = emailCliente;
-            this.passwordCliente = passwordCliente;
             this.direccionCliente = direccionCliente;
             this.telefonoCliente = telefonoCliente;
         }
@@ -50,7 +47,6 @@ namespace VentasJHJ.Model.Domain
         public string ApellidosCliente { get => apellidosCliente; set => apellidosCliente = value; }
         public string CedulaCliente { get => cedulaCliente; set => cedulaCliente = value; }
         public string EmailCliente { get => emailCliente; set => emailCliente = value; }
-        public string PasswordCliente { get => passwordCliente; set => passwordCliente = value; }
         public string DireccionCliente { get => direccionCliente; set => direccionCliente = value; }
         public string TelefonoCliente { get => telefonoCliente; set => telefonoCliente = value; }
         public List<PosOrden> Ordenes { get => ordenes; set => ordenes = value; }
